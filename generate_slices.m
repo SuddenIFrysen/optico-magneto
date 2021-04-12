4607d7a21193cd5a521b947007dec4d3ef203a34
Gustav Lidberg <gustav98.lidberg@gmail.com>
2021-04-12 16:50:44 +0200

    Slices are now a function!

--- generate_slices.m
+++ generate_slices.m
@@ -1,16 +1,20 @@
-%% Attempt at dynamic geometry
-r2 = 40;
-r1 = 25;
-h = 1;
-H = 5;
-L = 200;
-cyl1 = multicylinder([r1 r2], h, 'Void', [true, false]);
-cyl1 = translate(cyl1, [0 0 H/2]);
-cyl2 = multicylinder([r1 r2], h, 'Void', [true, false]);
-cyl2 = translate(cyl2, [0, 0, -H/2-h]);
+function gm = generate_slices(r1, r2, h, H, L)
+    % GENERATE_SLICES
+    % Create and return DiscreteGeometry object with two thin hollow cylinders
+    % within a box. Parameters:
+    %   r1 - cylinder inner radius
+    %   r2 - cylinder outer radius
+    %   h  - cylinder height (ideally very small)
+    %   H  - distance between cylinders (outer - outer)
+    %   L  - large cube side length
 
-cube = multicuboid(L, L, L);
-translate(cube, [0 0 -L/2]);
-cube = addCell(cube, cyl1);
-gm = addCell(cube, cyl2);
-pdegplot(gm, 'FaceAlpha', 0.5)
+    cyl1 = multicylinder([r1 r2], h, 'Void', [true, false]);
+    cyl1 = translate(cyl1, [0, 0, H/2-h]);
+    cyl2 = multicylinder([r1 r2], h, 'Void', [true, false]);
+    cyl2 = translate(cyl2, [0, 0, -H/2]);
+
+    cube = multicuboid(L, L, L);
+    translate(cube, [0 0 -L/2]);
+    cube = addCell(cube, cyl1);
+    gm = addCell(cube, cyl2);
+end
