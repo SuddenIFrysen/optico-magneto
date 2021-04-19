fbf8baeed8a90f3224265faceb1bfc01595ff14c
Kvarnberg6 <johanmillberg@me.com>
2021-04-19 16:45:14 +0200

    Added brand new and improved error catching functionality

--- okuja.m
+++ okuja.m
@@ -1,20 +1,36 @@
-function B = okuja(r_dipoles, m_matrix, r_eval)
+function B = okuja(r_dipoles, m_matrix, r_eval,r_thresh)
 % Lös alla dipolers B-fältsbidrag i den givna
 % För att få ut B-fält, summera allas bidrag
 
 % p_d: [[x1,y1,z1],[x2,y2,z2],..]
 % m_m: [[m_x1,m_y1,m_z1],[m_x2,m_y2,m_z2],..]
 % p_e: [[x1,y1,z1],[x2,y2,z2],..]
+% r_thresh: how close we allow the dipole to be to the point we
+% evaluate in
 
 mu_0 = 1.25663706212e-6;
 dipoles_amount = size(r_dipoles,1);
 position_amount = size(r_eval,1);
 
 B = zeros(position_amount,3);
-
+les_amount
+        r = r_eval(i,:)-r_dipoles(j,:);
+        if norm(r) < r_thresh
+            B(i,:) = [0,0,0];
+            break
+        end
+        B(i,:) = B(i,:) + (mu_0/(4*pi))*((3*r* ...
+            (m_matrix(j,:)*r'))/(norm(r))^5 - ...
+             m_matrix(j,:)/(norm(r))^3);
+    end
+end
 for i = 1:position_amount
     for j = 1:dipoles_amount
         r = r_eval(i,:)-r_dipoles(j,:);
+        if norm(r) < r_thresh
+            B(i,:) = [0,0,0];
+            break
+        end
         B(i,:) = B(i,:) + (mu_0/(4*pi))*((3*r* ...
             (m_matrix(j,:)*r'))/(norm(r))^5 - ...
              m_matrix(j,:)/(norm(r))^3);
