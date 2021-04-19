211834567ee7301dd7a922bc59e50ba0c1d588ae
Karl Pontén <karlponten@hotmail.com>
2021-04-19 16:21:30 +0200

    Added function for vector inner-product, needs expanding, but can handle vector-fields as function handles

---
+++ inner_prod.m
@@ -0,0 +1,18 @@
+function i = inner_prod(B1, B2)
+    % INNER_PROD
+    % Takes two vector-fields and calculates the GJK-Product©.
+    %   B1 - First vector-field to multiply. Either function handle(r,theta
+    %       )or evaluted in points as (n x 2) matrix with r pos in first 
+    %       column and theta in secound column.
+    %   B2 - Secound vector-field to multiply. Either function handle(r,theta
+    %       )or evaluted in points as (n x 2) matrix with r pos in first 
+    %       column and theta in secound column.
+    
+if class(B1) == 'function_handle' && class(B2) == 'function_handle'
+    f = @(r, t) r*dot(B1(r, t), B2(r,t));
+    i = integral2(f, 0, R, 0, 2*pi);
+end
+
+
+end
+
