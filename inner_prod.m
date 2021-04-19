600dd08a4f5d6f5e1e5aeb063c416f1d525c76cf
Karl Pontén <karlponten@hotmail.com>
2021-04-19 16:27:41 +0200

    Added R. Arrr like a pirate

--- inner_prod.m
+++ inner_prod.m
@@ -8,6 +8,8 @@ function i = inner_prod(B1, B2)
     %       )or evaluted in points as (n x 2) matrix with r pos in first 
     %       column and theta in secound column.
     
+    R = 1;
+    
 if class(B1) == 'function_handle' && class(B2) == 'function_handle'
     f = @(r, t) r*dot(B1(r, t), B2(r,t));
     i = integral2(f, 0, R, 0, 2*pi);
