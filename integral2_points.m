d0e0a96f723a758aaeca03938f07617de39d13d2
Karl Pontén <karlponten@hotmail.com>
2021-04-20 16:17:15 +0200

    summed integral better

--- integral2_points.m
+++ integral2_points.m
@@ -5,12 +5,6 @@ function I = integral2_points(val_in_points, dx, dy)
 if dy == 0
     dy = 2*pi/height(val_in_points);
 end
-I = 0;
-for i = 1:length(val_in_points)
-    for j = 1:height(val_in_points)
-        I = I + dx*dy*val_in_points(i,j);
-    end
-end
-
+I = dx*dy*all(val_in_points);
 end
 
