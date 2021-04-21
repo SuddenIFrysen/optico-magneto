226c8956abde2f7e3c2b40eacc8b113e1a12e0f7
Karl Pontén <karlponten@hotmail.com>
2021-04-21 14:07:52 +0200

    added  mmissing ;s

--- inner_product.m
+++ inner_product.m
@@ -4,11 +4,11 @@ function f = inner_product(R, origo, phi)
 %   origo - The center point of the graphene, as a (3 x 1) vector.
 %   phi - The angle which the graphene is rotated around the î-vector
 
-f = @(B1, B2) integra(B1, B2, R, origo, phi)
+f = @(B1, B2) integra(B1, B2, R, origo, phi);
 end
 
 function i = integra(B1, B2, R, origo, phi)
-    integrand_kart = @(x, y, z) dot(B1(x, y, z), B2(x, y, z), 3)
-    integrand_circ = @(r, t) r*integral_kart(r.*cos(t)+origo(1), r.*sin(t)*cos(phi)+origo(2), r.*sin(t)*sin(phi));
+    integrand_kart = @(x, y, z) dot(B1(x, y, z), B2(x, y, z), 3);
+    integrand_circ = @(r, t) r*integrand_kart(r.*cos(t)+origo(1), r.*sin(t)*cos(phi)+origo(2), r.*sin(t)*sin(phi));
     i = integral2(integrand_circ, 0, R, 0, 2*pi);
 end
