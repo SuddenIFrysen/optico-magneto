e61caf220744732decbab21bd19f6d44c3f767e5
Gustav Lidberg <gustav98.lidberg@gmail.com>
2021-04-26 17:14:39 +0200

    Implemented more flexibility in M variable specification

--- insert_params.m
+++ insert_params.m
@@ -1,15 +1,18 @@
-function M = insert_params(x,M_temp)
+function M = insert_params(x, M_vars, M_coeff, M_consts)
 %INSERT_PARAMS - Insert params into array
 % INPUTS:
-%   x      - 1 x n vector containing scalar parameter values
-%   M_temp - Array of any size, with n amount of NaN-values, where x(i) are
-%            inserted.
-    M = M_temp;
-    i = 1;
-    for j = 1:numel(M)
-        if isnan(M(j))
-            M(j) = x(i);
-            i = i + 1;
+%   x        - 1 x n vector containing scalar parameter values
+%   M_vars   - Array of any size, with n non-zero components pointing to an
+%   index of a parameter row vector x
+%   M_coeff  - Array of same size as M_vars, containing numerical
+%   coefficients to multiply variables above
+%   M_consts - Array of same size as M_vars, containing numerical values
+%   for those indices not set in M_vars
+    M = M_consts;
+    
+    for i = 1:numel(M_consts)
+        if M_vars(i) ~= 0
+            M(i) = M(i) + x(M_vars(i))*M_coeff(i);
         end
     end
 end
