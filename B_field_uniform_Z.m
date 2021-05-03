8dc45672991f27fc7eaa415dbd0bf247533b9ad4
Karl Pontén <karlponten@hotmail.com>
2021-05-03 14:59:11 +0200

    commented the code

--- B_field_uniform_Z.m
+++ B_field_uniform_Z.m
@@ -1,4 +1,7 @@
 function b = B_field_uniform_Z(target_domain, Str)
 %B_field_uniform_Z
+%   Gives back a uniform strength field in the normal direction to the 
+%   graphene. 
+%   Str - The value of the magnetic field. 
 b = @(x,y,z) Str*[0,-sin(target_domain.Phi),cos(target_domain.Phi)];
 end
