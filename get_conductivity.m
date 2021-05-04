59533efa9bf627e0f7d3febef505361ae0342ba8
Karl Pontén <karlponten@hotmail.com>
2021-05-04 14:24:47 +0200

    fixed conductivity

--- get_conductivity.m
+++ get_conductivity.m
@@ -1,10 +1,13 @@
 function sigma = get_conductivity(domain,B, omega,x,y,z)
-%GET_CONDUCTIVITY Summary of this function goes here
-%   Detailed explanation goes here
+%GET_CONDUCTIVITY 
+%   evalutas the the conductivity at x,y,z
+%   domain  - an object of the graphene class
+%   omega   - the wavelength of inclining light
+%   x,y,z   - each a scalar for the position.
 q = 1.60217662e-19;
 h_bar = 6.62607015e-34;
 c = 299792458;
-mass = 1;
+m = 1;
 M = m*domain.Fermi_vel^2/domain.Chem_pot;
 omega_c = q*B(x,y,z)/(m*c)*M;
 sigmaxx = q^2*domain.Chem_pot*1i*(omega-1i/domain.Tau)/(pi*h_bar*(omega-1i/domain.Tau...
