d4cf58136bc73541f7de863a243b9ad808c61e65
Gustav Lidberg <gustav98.lidberg@gmail.com>
2021-05-06 12:01:45 +0200

    linear example and breaking change in B_field_linear_any

--- B_field_linear_any.m
+++ B_field_linear_any.m
@@ -1,8 +1,10 @@
-function b = B_field_linear_any(target_domain, Str, dir)
+function b = B_field_linear_any(target_domain, Str, a, dir)
 %B_field_linear
 %Generates a field with a linear profile over the graphene.
 %   target_domain as aa struct detailed in main.mlx
 %   Str as a scalar and described in eq below
+%   a an offset term
+%   Field strength equal to Str * (a + x) ,  x in [-1, 1]
 
-b = @(X,Y,Z) Str * permute(target_domain.Normal, [1 3 2]) .* (X * dir(1) + Y * dir(2) + Z * dir(3)) ./ target_domain.R;
+b = @(X,Y,Z) Str * permute(target_domain.Normal, [1 3 2]) .* (a+(X * dir(1) + Y * dir(2) + Z * dir(3)) ./ target_domain.R);
 end
